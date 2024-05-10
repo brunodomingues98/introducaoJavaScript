5b3fca1d72b297eafea8b9f09de845ae0a9144f0
Bruno Domingues <p.brunodomingues@gmail.com>
2024-05-10 15:10:07 -0300

    fechando parte de arrays com os metodos .concat() e .flatMap()

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Maria', 'Ana'];
+const filhos = ['João', 'Pedro'];
+const todos = filhas.concat(filhos, 'Carlos')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
