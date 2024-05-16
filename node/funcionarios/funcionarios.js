c7140d64fb945a1dec9bb664f3e8d5844c93f984
Bruno Domingues <p.brunodomingues@gmail.com>
2024-05-16 15:13:39 -0300

    resolvendo desafio usando filter

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,7 +1,35 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+const frances = f => f.pais === 'France'
+const homem = f => f.genero === 'M'
+const maiorSalario = (func1, funcAtual1) => {
+    return func1.salario > funcAtual1.salario ? func1 : funcAtual1
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
     console.log(funcionarios)
+
+    // mulher chinesa com menor salario
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .filter(menorSalario)
+
+    console.log(func)
+
+    // homem frances com maior salario
+    const func1 = funcionarios
+        .filter(frances)
+        .filter(homem)
+        .filter(maiorSalario)
+
+    console.log(func1)
 })
