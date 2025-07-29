15eaab21f2c2c4bfb4fc79f37ea71cd92883f018
Bruno Domingues <p.brunodomingues@gmail.com>
2025-07-29 11:47:20 -0300

    finalizando promise

---
+++ esnext/promise_05.js
@@ -0,0 +1,14 @@
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        if(Math.random() < chanceErro) {
+            reject('Ocorreu erro!')
+        } else {
+            resolve(valor)
+        }
+    })
+}
+
+funcionarOuNao('Testando...', 0.5)
+    .then(v => console.log(`Valor: ${v}`))
+    .catch(err => console.log(`Erro: ${err}`))
+    .then(() => console.log('Fim!'))
